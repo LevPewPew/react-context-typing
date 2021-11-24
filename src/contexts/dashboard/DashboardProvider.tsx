1724517f1c408d0459ef6a48336d0faab4f7eb90
LevPewPew <levthedev@protonmail.com>
2021-11-24 16:57:44 +1100

    updated README. added some comments

--- src/contexts/dashboard/DashboardProvider.tsx
+++ src/contexts/dashboard/DashboardProvider.tsx
@@ -2,6 +2,9 @@ import { createContext, useReducer } from "react";
 import reducer from "./reducer";
 import { Action, Dashboard } from "./types.d";
 
+/* undefined! is a way to assert a non-null type. This is useful for avoiding 
+passing an initial value when we know that the context would not be used without
+a provider (which is pretty much always...) */
 export const context = createContext<
   [state: Dashboard, dispatch: React.Dispatch<Action>]
 >(undefined!);
@@ -10,6 +13,7 @@ const initialState: Dashboard = { kind: "car" };
 
 function DashboardProvider({ children }: { children: React.ReactNode }) {
   const [state, dispatch] = useReducer(reducer, initialState);
+  /* explicit tuple type because TS's implicit type will be an array of unions */
   const value: [Dashboard, React.Dispatch<Action>] = [state, dispatch];
 
   return <context.Provider value={value}>{children}</context.Provider>;
--- src/contexts/foobar/FoobarProvider.tsx
+++ src/contexts/foobar/FoobarProvider.tsx
@@ -2,6 +2,9 @@ import { createContext, useReducer } from "react";
 import reducer from "./reducer";
 import { Action, Foobar } from "./types.d";
 
+/* undefined! is a way to assert a non-null type. This is useful for avoiding 
+passing an initial value when we know that the context would not be used without
+a provider (which is pretty much always...) */
 export const context = createContext<
   [state: Foobar, dispatch: React.Dispatch<Action>]
 >(undefined!);
@@ -10,6 +13,7 @@ const initialState: Foobar = { fizz: 0, buzz: 0 };
 
 function FoobarProvider({ children }: { children: React.ReactNode }) {
   const [state, dispatch] = useReducer(reducer, initialState);
+  /* explicit tuple type because TS's implicit type will be an array of unions */
   const value: [Foobar, React.Dispatch<Action>] = [state, dispatch];
 
   return <context.Provider value={value}>{children}</context.Provider>;
