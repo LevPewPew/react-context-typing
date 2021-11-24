b0dc7aa21896873eecaec3788f97fc381a7af0e1
LevPewPew <levthedev@protonmail.com>
2021-11-24 16:37:35 +1100

    fixed missed typo, that was not seen from hot reload or watching! look into why hot reload broken...

--- src/App.tsx
+++ src/App.tsx
@@ -22,23 +22,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>{dashboardState.kind}</h1>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Dashboard: {dashboardState.kind.toUpperCase()}</h1>
         <br />
         <div style={{ display: "flex" }}>
           <Fizz />
           <Buzz />
         </div>
+        <br />
         <br />
         <DashboardCycler />
       </header>
---
+++ src/foobar/foobar.tsx
@@ -0,0 +1,52 @@
+import { createContext, useReducer } from "react";
+
+interface Foobar {
+  fizz: number;
+  buzz: number;
+}
+
+type FoobarActionKindWithPayload = "SET_FIZZ" | "SET_BUZZ";
+
+type FoobarActionKindNoPayload = "INCREMENT_FIZZ" | "INCREMENT_BUZZ";
+
+type FoobarAction = FoobarActionWithPayload | FoobarActionNoPayload;
+
+interface FoobarActionWithPayload {
+  type: FoobarActionKindWithPayload;
+  payload: number;
+}
+
+interface FoobarActionNoPayload {
+  type: FoobarActionKindNoPayload;
+}
+
+function foobarReducer(state: Foobar, action: FoobarAction) {
+  switch (action.type) {
+    case "SET_FIZZ":
+      return { ...state, fizz: action.payload };
+    case "INCREMENT_FIZZ":
+      return { ...state, fizz: state.fizz + 1 };
+    case "SET_BUZZ":
+      return { ...state, buzz: action.payload };
+    case "INCREMENT_BUZZ":
+      return { ...state, buzz: state.buzz + 1 };
+    default:
+      return state;
+  }
+}
+
+export const foobarContext = createContext<
+  [Foobar, React.Dispatch<FoobarAction>]
+>(undefined!);
+
+const initialState: Foobar = { fizz: 0, buzz: 0 };
+
+export function FoobarProvider({ children }: { children: React.ReactNode }) {
+  const [state, dispatch] = useReducer(foobarReducer, initialState);
+  // must provide this type, because TS implicitly things this is an array that could take values of either the state or the dispatch, but it is infact a tuple
+  const value: [Foobar, React.Dispatch<FoobarAction>] = [state, dispatch];
+
+  return (
+    <foobarContext.Provider value={value}>{children}</foobarContext.Provider>
+  );
+}
